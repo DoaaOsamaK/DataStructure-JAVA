82abbc745f3938a8f3cf04828f7998f702e946b4
D U A A O <147305995+DoaaOsamaK@users.noreply.github.com>
2024-09-29 22:24:25 -0700

    add indexOf method

--- MyArrayList.java
+++ MyArrayList.java
@@ -102,6 +102,15 @@ public class MyArrayList<Integer> {
             return null;
         }
     }
+
+     public int indexOf(Integer val) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] != null && arr[i].equals(val)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
     
     
