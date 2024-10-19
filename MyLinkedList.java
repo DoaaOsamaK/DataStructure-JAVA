2e4fed4ce0990951d1406ebfb46e896e0e5d5393
D U A A O <147305995+DoaaOsamaK@users.noreply.github.com>
2024-10-18 23:31:09 -0700

    add(int index, D val) Func

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -36,6 +36,28 @@ public class MyLinkedList {
     }
     size++; 
 }
-      
+      public void add(int index, D val) {
+    if (index < 0 || index > size) {
+        throw new ArrayIndexOutOfBoundsException("Invalid index: " + index + ", Size: " + size);
+    }
+
+    if (index == 0) {
+        addFirst(val);  
+    } else if (index == size) {
+        add(val);  
+    } else {
+        Node<D> newNode = new Node<>(val);
+        Node<D> current = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+}
+
 
 }
