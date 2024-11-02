6892a63047385bd1cf8740849dd958d2d41e066b
D U A A O <147305995+DoaaOsamaK@users.noreply.github.com>
2024-11-02 07:42:35 +0200

    Create MyDoublyLinkedList.java

---
+++ MyDoublyLinkedList.java
@@ -0,0 +1,15 @@
+public class MyDoublyLinkedList {
+    private class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private Node head;
+}
