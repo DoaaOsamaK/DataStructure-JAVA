63bfccbfa93199a90b7dc6a9b8068ae51b66cda4
D U A A O <147305995+DoaaOsamaK@users.noreply.github.com>
2024-11-02 07:45:23 +0200

    Method to display the list in forward direction

--- MyDoublyLinkedList.java
+++ MyDoublyLinkedList.java
@@ -59,4 +59,13 @@ public class MyDoublyLinkedList {
         }
     }
 
+    public void displayForward() {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
 }
